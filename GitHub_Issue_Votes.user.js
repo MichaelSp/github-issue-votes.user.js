a1d46a10598c18dd533c8c0194379fe6f2a5dad4
Michael Sprauer <Michael@Sprauer.net>
2015-08-03 15:17:54 +0200

    Initial commit

---
+++ GitHub_Issue_Votes.user.js
@@ -0,0 +1,98 @@
+// ==UserScript==
+// @name           GitHub: Issue Votes
+// @namespace      https://github.com/MichaelSp/github-issue-votes.user.js
+// @description    A user script to display user votes when browsing Github issues.
+// @include        https://github.com/*/*/issues
+// @include        https://github.com/*/*/issues/*
+// @include        https://github.com/*/*/pull/*
+// @run-at         document-end
+// @grant          none
+// @icon           http://skratchdot.com/favicon.ico
+// @downloadURL    https://github.com/MichaelSp/github-issue-votes.user.js/raw/master/github-issue-vptes.user.js
+// @updateURL      https://github.com/MichaelSp/github-issue-votes.user.js/raw/master/github-issue-votes.user.js
+// @version        1.9
+// ==/UserScript==
+
+/*global jQuery */
+/*jslint browser: true */
+
+(function () {
+    'use strict';
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+
+    addGlobalStyle('\
+    .vote_positive { background-color: #6CC644; color: #FFF; }\
+    .vote_negative { background-color: #BD2C00; color: #FFF; }\
+    ');
+
+    var scanForVotes = function () {
+        var positive = new Set();
+        var negative = new Set();
+        jQuery('.comment-body').each(function () {
+            var elem = jQuery(this);
+            var plainVote, name, imageVoteUp, contextVote, only_p = undefined;
+            try {
+                name = elem.parents('.timeline-comment-wrapper').find('>a').attr('href').replace(/^\//, '@');
+                imageVoteUp = elem.find('img[alt=":+1:"], img[alt=":thumbsup:"]');
+                contextVote = /\+1/.test(elem.text());
+                if (contextVote || imageVoteUp.length) {
+                    elem.parents('.timeline-comment-wrapper').find('.timeline-comment-header').addClass('vote_positive').removeClass('vote_negative');
+                    positive.add(name);
+                    negative.delete(name);
+                    only_p = elem.find('> p:only-child');
+                    plainVote = /^\s*(\+1|:\+1:)\s*$/.exec(only_p.text());
+                    var collapse = plainVote.length>0 || only_p.find('> img:only-child').length>0;
+                    if (collapse) {
+                        var hint = plainVote.length > 0 ? plainVote[0] : elem.find('img').html();
+                        var comment = elem.parents('.comment');
+                        var header = comment.find('.timeline-comment-header-text');
+                        header.html(header.html().replace(/commented/, 'voted (' + hint + ')'));
+                        comment.find('.comment-content').remove();
+                    }
+                }
+                else if (/-1/.test(elem.text()) || elem.find('img[alt=":-1:"], img[alt=":thumbsdown:"]').length>0) {
+                    elem.parents('.timeline-comment-wrapper').find('.timeline-comment-header').addClass('vote_negative').removeClass('vote_positive');
+                    negative.add(name);
+                    positive.delete(name);
+                }
+            }
+            catch(e){}
+        });
+        return {positive: positive, negative: negative};
+    };
+
+    var addVoteSection = function(voters) {
+        var names = Array.from(voters.positive).join(',\n');
+        var votesHTML = '<div class="discussion-sidebar-item sidebar-votes js-discussion-sidebar-item"> \
+            <h3 class="discussion-sidebar-heading"> \
+                Votes \
+            </h3> \
+            <div class="issues css-truncate">';
+        var add = '<span id="votes_counter" class="counter" title="'+names+'">+'+voters.positive.size+'</span>';
+        var neg = '<span id="votes_counter" class="counter" title="'+names+'">-'+voters.negative.size+'</span>';
+        var abs = '<span id="votes_counter" class="counter" title="'+names+'">'+(voters.positive.size-voters.negative.size)+'</span>';
+        votesHTML += (voters.positive.size+voters.negative.size) == 0 ? 'None yet' : add + neg + "=" +abs;
+        votesHTML += '</div></div>';
+        jQuery('.discussion-sidebar-item.sidebar-labels').before(votesHTML);
+    };
+
+    var init = function () {
+        var voters = scanForVotes();
+        addVoteSection(voters);
+    };
+
+    jQuery(document).ready(function () {
+        jQuery(document).on('pjax:end', function (event) {
+            init();
+        });
+        init();
+    });
+}());
---
+++ README.md
@@ -0,0 +1,29 @@
+User Script : GitHub Issue Votes
+================================
+
+### Description ###
+
+This is a user script that adds a voting feature to Github issues
+and pull requests.
+
+Just comment `+1` or `-1`.
+
+### Installation ###
+
+1. Make sure you have user scripts enabled in your browser (these instructions refer to the latest versions of the browsers):  
+    * ***CHROME 21+***:
+      1. Download the user script.
+      2. Open **chrome://chrome/extensions/**.
+      3. Drag and drop the user script file on the page you opened in step 2.
+    * ***CHROME 20 (and below)***: User scripts are enabled by default. Continue to STEP 2.
+    * ***FIREFOX***: Install [GreaseMonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/), then continue to STEP 2.
+    * ***IE***: Install [Trixie](http://www.bhelpuri.net/Trixie/). Continue to STEP 2.
+    * ***OPERA***: Follow instructions located on Opera's site: [User JS](http://www.opera.com/docs/userjs/). Continue to STEP 2.
+    * &#x20;<del>***SAFARI***: Install [SIMBL](http://www.culater.net/software/SIMBL/SIMBL.php). Install [GreaseKit](http://8-p.info/greasekit/). Continue to STEP 2.</del>
+    * ***SAFARI***: Install [NinjaKit](http://d.hatena.ne.jp/os0x/20100612/1276330696). Continue to STEP 2.
+2. Install the "GitHub Issue Votes" user script by clicking here: [github-issue-votes.user.js](https://github.com/MichaelSp/github-issue-votes.user.js/raw/master/github-issue-votes.user.js).  
+
+### Screenshots ###
+
+TODO
+  
